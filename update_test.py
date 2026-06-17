3ca6d2244f917cecec2d39c782232c70cf931fd3
Brad Pitcher <bradpitcher@gmail.com>
2026-06-16 19:44:44 -0700

    Delete update_test.py

--- update_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('apps/readest-app/src/__tests__/helpers/updater.test.ts', 'r') as f:
-    content = f.read()
-
-content = content.replace("getAppVersion: () => mockAppVersion,", "getAppVersion: () => mockAppVersion,\n  isNightly: () => mockAppVersion.includes('nightly'),")
-
-with open('apps/readest-app/src/__tests__/helpers/updater.test.ts', 'w') as f:
-    f.write(content)
